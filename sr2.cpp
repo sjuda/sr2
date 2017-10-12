60240df2a546ceb53427f4604d0a3df633915e2b
sjuda <32189174+sjuda@users.noreply.github.com>
2017-10-12 18:25:53 +0300

    Update and rename sr2 to sr2.cpp

---
+++ sr2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+// Задача 7 (Вариант 15)
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    int c;
+    cout << "Enter value: ";
+    cin >> a >> b >> c;
+    cout << "Result is " << b << a << c;
+    return 0;
+}
